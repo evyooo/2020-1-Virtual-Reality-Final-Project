83097cdb84e18ee8d64e1fee977aac58cc051ef8
evyooo <yooooojining@gmail.com>
2020-06-27 22:43:42 +0900

    Create Scripts

---
+++ Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+
+    private Text scoreText;
+    public string LevelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		scoreText = GetComponent<Text> ();
+
+    }
+
+     // Update is called once per frame
+    void Update () 
+    {
+        scoreText.text = "ITEM FOUND " + GlobalVariable.score.ToString("f0") + "/10";
+        
+        if (GlobalVariable.score >= 10){
+            Application.LoadLevel (LevelToLoad);
+
+        }
+
+	}
+    
+}
---
+++ Scripts/foundkit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foundkit : MonoBehaviour
+{
+    public GameObject cube;
+    public AudioSource audioSource;
+    public AudioClip bgm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = bgm; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnMouseDown()
+    { 
+    	
+    	GlobalVariable.score += 1;
+    	Destroy(cube);
+        
+        audioSource.Play();
+        audioSource.loop = false; //반복 여부
+    }
+
+}
